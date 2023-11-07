f04fb2eeaefe2eb54ae0c0cc0c72c9cffa9230d4
RDSMatheus <matheus_rds1998@hotmail.com>
2023-11-07 07:52:11 -0300

    bug fix

--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const MailController = require('../../controller/MailController.js');
+const MailController = require('../controller/MailController.js');
 
 router.post('/sendmail', MailController.mailSend);
 
