429432411c3096c1be77dd59bf2fffef2e70ae5a
RDSMatheus <matheus_rds1998@hotmail.com>
2023-11-07 18:25:36 -0300

    troca de nodemailer por resend

--- controller/MailController.js
+++ controller/MailController.js
@@ -1,54 +1,29 @@
 require('dotenv').config();
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 const EMAIL = process.env.EMAIL;
-const PASSWORD = process.env.PASSWORD;
+const API_KEY_RESEND = process.env.API_KEY_RESEND;
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp-mail.outlook.com',
-  port: 587,
-  secure: false,
-  auth: {
-    user: EMAIL,
-    pass: PASSWORD,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: 'smtp-mail.outlook.com',
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: EMAIL,
+//     pass: PASSWORD,
+//   },
+// });
 
 module.exports = class MailController {
   static async mailSend(req, res) {
     const { remetente, assunto, mensagem } = req.body;
 
-    await new Promise((resolve, reject) => {
-      // verify connection configuration
-      transporter.verify(function (error, success) {
-        if (error) {
-          console.log(error);
-          reject(error);
-        } else {
-          console.log('Server is ready to take our messages');
-          resolve(success);
-        }
-      });
-    });
+    const resend = new Resend(API_KEY_RESEND);
 
-    const mailOptions = {
-      from: EMAIL,
-      to: EMAIL,
+    resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: 'contatostrongfit@outlook.com',
       subject: assunto,
-      text: `<h1>${remetente}<h1>
-      <p>${mensagem}</p>`,
-    };
-
-    await new Promise((resolve, reject) => {
-      // send mail
-      transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-          console.error(err);
-          reject(err);
-        } else {
-          console.log(info);
-          resolve(info);
-        }
-      });
+      html: mensagem,
     });
   }
 };
