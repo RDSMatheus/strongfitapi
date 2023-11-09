0abcb0d0b3d16785ef8f97a627c82b2a6abac8ac
RDSMatheus <matheus_rds1998@hotmail.com>
2023-11-09 12:03:07 -0300

    cors test

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const ORIGIN = process.env.ORIGIN;
 
 const corsOptions = {
   origin: ORIGIN,
-  methods: 'POST',
-  allowedHeaders: 'Content-Type, Authorization',
 };
 
 app.use(cors(corsOptions));
